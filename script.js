f8f95e705b01bef1ef7ada41782a703eebe295db
adammchairil <rifaichairil11@gmail.com>
2025-04-24 21:58:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,154 @@
+function saklar(action, lamp) {
+    let toggle1 = document.getElementById("toggle-default1");
+    let toggle2 = document.getElementById("toggle-default2");
+    let toggle3 = document.getElementById("toggle-default3");
+    let toggle4 = document.getElementById("toggle-default4");
+    let toggle5 = document.getElementById("toggle-default5");
+    let toggle6 = document.getElementById("toggle-default6");
+    let toggle7 = document.getElementById("toggle-default7");
+    let toggle8 = document.getElementById("toggle-default8");
+    let toggle9 = document.getElementById("toggle-default9");
+    let toggle10 = document.getElementById("toggle-default10");
+    let toggle11 = document.getElementById("index-toggle1");
+    let toggle12 = document.getElementById("index-toggle2");
+    let toggle13 = document.getElementById("index-toggle3");
+    let toggle14 = document.getElementById("index-toggle4");
+   
+
+    let lampu1 = document.getElementById("LAMPU-1");
+    let lampu2 = document.getElementById("LAMPU-2");
+    let lampu3 = document.getElementById("LAMPU-3");
+    let lampu4 = document.getElementById("LAMPU-4");
+    let lampu5 = document.getElementById("LAMPU-5");
+    let lampu6 = document.getElementById("LAMPU-6");
+    let lampu7 = document.getElementById("LAMPU-7");
+    let lampu8 = document.getElementById("LAMPU-8");
+    let lampu9 = document.getElementById("LAMPU-9");
+    let lampu10 = document.getElementById("LAMPU-10");
+    let lampu11 = document.getElementById("LAMPU-11");
+    let lampu12 = document.getElementById("LAMPU-12");
+    let lampu13 = document.getElementById("LAMPU-13");
+    let lampu14 = document.getElementById("LAMPU-14");
+    let lampu15 = document.getElementById("LAMPU-15");
+    let lampu16 = document.getElementById("LAMPU-16");
+    let lampu17 = document.getElementById("LAMPU-17");
+    let lampu18 = document.getElementById("LAMPU-18");
+    let lampu19 = document.getElementById("LAMPU-19");
+    let lampu20 = document.getElementById("LAMPU-20");
+
+
+    // LAMPU SATUAN
+    if(toggle1.checked) {
+        lampu1.src= "Assets/Images/on.gif";
+    }
+    else {
+        lampu1.src= "Assets/Images/off.gif";
+     }
+     if(toggle2.checked) {
+        lampu2.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu2.src= "Assets/Images/off.gif";
+     }
+     if(toggle3.checked) {
+        lampu3.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu3.src= "Assets/Images/off.gif";
+     }
+     if(toggle4.checked) {
+        lampu4.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu4.src= "Assets/Images/off.gif";
+     }
+     if(toggle5.checked) {
+        lampu5.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu5.src= "Assets/Images/off.gif";
+     }
+     if(toggle6.checked) {
+        lampu6.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu6.src= "Assets/Images/off.gif";
+     }
+     if(toggle7.checked) {
+        lampu7.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu7.src= "Assets/Images/off.gif";
+     }
+     if(toggle8.checked) {
+        lampu8.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu8.src= "Assets/Images/off.gif";
+     }
+     if(toggle9.checked) {
+        lampu9.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu9.src= "Assets/Images/off.gif";
+     }
+     if(toggle10.checked) {
+        lampu10.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu10.src= "Assets/Images/off.gif";
+     }
+     if(toggle11.checked) {
+        lampu11.src= "Assets/Images/on.gif";
+        lampu12.src= "Assets/Images/on.gif";
+        lampu13.src= "Assets/Images/on.gif";
+        lampu14.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu11.src= "Assets/Images/off.gif";
+        lampu12.src= "Assets/Images/off.gif";
+        lampu13.src= "Assets/Images/off.gif";
+        lampu14.src= "Assets/Images/off.gif";
+     }
+     if(toggle12.checked) {
+        lampu15.src= "Assets/Images/on.gif";
+        lampu16.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu15.src= "Assets/Images/off.gif";
+        lampu16.src= "Assets/Images/off.gif";
+     }
+     if(toggle13.checked) {
+        lampu17.src= "Assets/Images/on.gif";
+        lampu18.src= "Assets/Images/on.gif";
+        lampu19.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu17.src= "Assets/Images/off.gif";
+        lampu18.src= "Assets/Images/off.gif";
+        lampu19.src= "Assets/Images/off.gif";
+     }
+     if(toggle14.checked) {
+        lampu20.src= "Assets/Images/on.gif";
+     }
+     else {
+        lampu20.src= "Assets/Images/off.gif";
+     }
+
+     // Mengambil elemen yang ingin dipindahkan
+
+    // const lampu1 = document.getElementById('LAMPU-11'); // Ganti dengan ID elemen yang ingin dipindahkan
+    // const lampu11 = document.getElementById('LAMPU-1'); // Ganti dengan ID tumpukan target
+
+    // // Memeriksa apakah elemen yang ingin dipindahkan ada
+    // if (toggle11.checked) {
+    // // Memindahkan elemen ke tumpukan paling atas
+    // targetStack.appendChild(lampu1);
+    // lampu11.src= "Assets/Images/on.gif"
+    // ;
+    // } else {
+    // console.log('Elemen yang ingin dipindahkan tidak ditemukan.');
+    // }
+    
+
+}
